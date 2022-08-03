fd8e2a7e49c3baacb34efca1feab1760231a8bf9
TarsilaNascMartins <tarsilawho@gmail.com>
2022-08-03 20:43:45 -0300

    script

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -4,4 +4,15 @@ Create Table Empresa (id INT PRIMARY KEY, Nome VARCHAR (255), CNPJ INT, Endereco
 
 Create Table Pais (Id INT PRIMARY KEY, Nome VARCHAR (255))
 
+Create Table Cliente (CPF INT PRIMARY KEY, NOME VARCHAR (255), Telefone INT)
+
+Create Table Banco (Id INT PRIMARY KEY, Nome VARCHAR (255),Tp_Conta VARCHAR (255))
+
+Create Table Moeda ( Id INT PRIMARY KEY, Nome VARCHAR)
+
+Create Table Contas_Contabeis (Id INT PRIMARY KEY, Lucro MONEY, Despesas MONEY)
+
+Create Table Funcionarios (CPF INT PRIMARY KEY, Nome VARCHAR (255))
+
+Create Table Projetos (Id INT PRIMARY KEY, Nome VARCHAR(255), CPF INT)
 
