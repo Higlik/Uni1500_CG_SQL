d02b9e8efa3833061475ba9c0e98649d90ec4e48
Victor Silva de Franca <victor.silva.franca18@gmail.com>
2022-08-03 20:29:23 -0300

    Initial commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+Create Database CG_UNI1500
+
+Create Table Empresa (id INT PRIMARY KEY, Nome VARCHAR (255), CNPJ INT, Endereco Varchar (255))
+
+Create Table Pais (Id INT PRIMARY KEY, Nome VARCHAR (255))
+
+
